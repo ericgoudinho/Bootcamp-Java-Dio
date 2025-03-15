6d39207281a3da3d06e465f20f32938a0513dfa3
Eric Goudinho <ericgoudinho5@gmail.com>
2025-03-15 20:05:06 -0300

    Adicionado exemplo de uso de variáveis em Java.

--- src/dio/com/java/variaveis/Variaveis.java
+++ src/dio/com/java/variaveis/Variaveis.java
@@ -3,5 +3,25 @@ package dio.com.java.variaveis;
 public class Variaveis {
     public static void main (String [] args) {
         System.out.println ("Hello World");
+
+        //Tipos primitivos
+        String meuNome = "Eric";
+        byte idade = 25;
+        short ano = 2025;
+        int cep = 98840808;
+        long cpf = 8742847982l;
+        System.out.println (cpf);
+
+        float pi = 3.14f;
+        double salario = 1340.45;
+
+        // Reatribuição de variável
+        int numero = 10;
+        numero = 4;
+        System.out.println (numero);
+
+        // Constantes (valores que não mudam)
+        final double VALOR_PI = 3.14;
+        System.out.println (VALOR_PI);
     }
 }
